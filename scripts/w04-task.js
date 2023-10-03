fa1f5e85bb41a3848001124ff2e7c64310dd0744
Tagg McDonald <taggmcd@gmail.com>
2023-10-03 11:13:56 -0600

    Week4 Tasks

---
+++ scripts/w04-task.js
@@ -0,0 +1,58 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+  name: "Tagg McDonald",
+  photo: "/images/profile.webp",
+  favoriteFoods: ["Smoked Brisket", "Ribs", "Sushi"],
+  hobbies: ["Mountain Biking", "Programming", "Hiking"],
+  placesLived: [],
+};
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+  {
+    place: "Pleasant Grove, UT",
+    length: "14 Years",
+  },
+  {
+    place: "Brigham City, UT",
+    length: "2 Years",
+  },
+  {
+    place: "St. George, UT",
+    length: "3 Years",
+  }
+);
+/* DOM Manipulation - Output */
+/* Name */
+document.querySelector("#name").textContent = myProfile.name;
+
+/* Photo with attributes */
+document.querySelector("#photo").src = myProfile.photo;
+document.querySelector("#photo").alt = myProfile.name;
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach((food) => {
+  let li = document.createElement("li");
+  li.textContent = food;
+  document.querySelector("#favorite-foods").appendChild(li);
+});
+
+/* Hobbies List */
+myProfile.hobbies.forEach((hobby) => {
+  let li = document.createElement("li");
+  li.textContent = hobby;
+  document.querySelector("#hobbies").appendChild(li);
+});
+
+/* Places Lived DataList */
+myProfile.placesLived.forEach((place) => {
+  let dt = document.createElement("dt");
+  let dd = document.createElement("dd");
+  dt.textContent = place.place;
+  dd.textContent = place.length;
+
+  document.querySelector("#places-lived").appendChild(dt);
+  document.querySelector("#places-lived").appendChild(dd);
+});
